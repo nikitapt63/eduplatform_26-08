c8b25896b68f1fd52162f4a04a52b644e94c7f1b
nikitapt63 <nikitapt63@gmail.com>
2024-12-23 20:09:51 +0300

    linters

--- eduplatform/eduplatform/settings.py
+++ eduplatform/eduplatform/settings.py
@@ -16,14 +16,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    *["django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles"],
-    *["rest_framework"]
-    *["users", "courses"]
+    *[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+    ],
+    *["rest_framework"], * ["users", "courses"],
 ]
 
 MIDDLEWARE = [
