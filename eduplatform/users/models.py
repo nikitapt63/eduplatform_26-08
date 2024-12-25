5e7018c7989bfc7ead62e1986fcaf4a64b5e8093
YanaUmantseva <yanumanceva@gmail.com>
2024-12-26 02:41:13 +0300

    добавление CustomUser

--- eduplatform/users/models.py
+++ eduplatform/users/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Email необходим для создания пользователя.')
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    role = models.CharField(
+        max_length=20,
+        choices=[
+            ('teacher', 'Teacher'),
+            ('student', 'Student'),
+            ('admin', 'Admin'),
+            ('moderator', 'Moderator'),
+        ],
+        default='student'
+    )
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
