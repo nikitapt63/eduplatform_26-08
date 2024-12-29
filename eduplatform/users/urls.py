8b4f269aa9bcb2c2df07d4ce131272d13ff3ffb3
nikitapt63 <nikitapt63@gmail.com>
2024-12-29 20:45:22 +0300

    add users.viewsets

---
+++ eduplatform/users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from .endpoints import CustomUserViewSetAPI, StudentViewSetAPI, TeacherViewSetAPI
+
+router = SimpleRouter()
+router.register("users", CustomUserViewSetAPI)
+router.register("students", StudentViewSetAPI)
+router.register("teachers", TeacherViewSetAPI)
+
+
+urlpatterns = [
+    path("", include(router.urls))
+]
